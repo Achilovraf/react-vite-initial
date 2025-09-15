4c1c58d803df7b7758530c01bc3591a461957076
Achilovraf <Achilovraf@gmail.com>
2025-09-15 10:20:53 +0500

    Add current year display with JSX and style comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+// src/App.jsx
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import ReactLogo from './assets/react.svg?react'
-export const App = () => (
-    <div>
-      <h1>Vite + React</h1>
-	  <ReactLogo/>
-    </div>
-  )
 
+export const App = () => {
+  const [count, setCount] = useState(0)
+// Декларативный стиль - описываем что хотим получить
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+		{/* Императивный стиль - описывает как изменить состояние */}
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+	  {/* Декларативное отображение года */}
+	  <p>® {currentYear}</p>
+    </>
+  )
+}
 
 
