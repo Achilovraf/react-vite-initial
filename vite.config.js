e9f0d504e18f9740ddc54521a8a716b2da6fc784
Achilovraf <Achilovraf@gmail.com>
2025-09-15 10:44:17 +0500

    Add current year display with JSX and style comments

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	esbuild: {
-		loader: 'jsx',
-		include: /src\/.*\.js$/,
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {
-				'.js': 'jsx',
-			},
-		},
-	},
 });
